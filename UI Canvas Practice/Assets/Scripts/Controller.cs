0289acf275858e9bdfc595dc7f94f5bc013233b8
장영찬 <kuroli777@gmail.com>
2017-08-25 14:38:10 +0900

    01. UI CANVAS

---
+++ UI Canvas Practice/Assets/Scripts/Controller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour {
+
+    public Text ResultText;
+	// Use this for initialization
+	void Start () {
+        ResultText.text = "aa";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Click()
+    {
+        ResultText.text = "bb";
+    }
+}
